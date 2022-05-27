da5c87ab55fb9dd42fcd216d36c4acae0e24d2b6
Luis Garcia <iam.luisg@gmail.com>
2022-05-27 18:47:13 +0200

    Initial Commit

---
+++ Auth0AsyncTest/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Auth0AsyncTest
+//
+//  Created by Luis Garcia on 5/27/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
